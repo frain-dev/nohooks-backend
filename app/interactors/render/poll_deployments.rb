b23b15d05927f419704b80150cab52a305c1db83
Subomi Oluwalana <subomioluwalana71@gmail.com>
2023-06-19 20:17:07 +0100

    fix: fixed autoloading bugs

--- app/interactors/render/poll_deployments.rb
+++ app/interactors/render/poll_deployments.rb
@@ -1,5 +1,5 @@
 module Render
-  class PollDeployment < ApplicationInteractor
+  class PollDeployments < ApplicationInteractor
     def call
       @account = context.account
       @client = RenderRuby::Client.new(api_key: @account.api_key)
--- app/jobs/poll_render_resources_job.rb
+++ app/jobs/poll_render_resources_job.rb
@@ -4,7 +4,7 @@ class PollRenderResourcesJob < ApplicationJob
   def perform(account_id)
     account = RenderAccount.find(account_id)
 
-    Render::PollJobs.call(account: account)
+    #Render::PollJobs.call(account: account)
     Render::PollService.call(account: account)
     Render::PollDeployments.call(account: account)
   end
