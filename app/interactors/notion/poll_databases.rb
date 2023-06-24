7733ec4c4cc0dd626586bbbb637091777c1f92a5
Subomi Oluwalana <subomioluwalana71@gmail.com>
2023-06-24 22:31:10 +0100

    feat: notion webhooks coming through

--- app/interactors/notion/poll_databases.rb
+++ app/interactors/notion/poll_databases.rb
@@ -12,7 +12,7 @@ module Notion
         next if rows.nil? || rows.empty?
 
         rows.each do |row|
-          computed_hash = compute_deployment_hash(deployment)
+          computed_hash = compute_row_hash(row)
           db_row = NotionRow.find_by_row_id(row.id)
 
           if db_row.nil?
@@ -25,7 +25,7 @@ module Notion
           end
         end
 
-        deleted_rows = retrieve_deleted_rows(rows)
+        deleted_rows = retrieve_deleted_rows(database, rows)
         next if deleted_rows.empty?
 
         deleted_rows.each do |db_row|
@@ -33,62 +33,63 @@ module Notion
         end
       end
 
-      private
+    end
 
-      def retrieve_rows(database, cursor: nil)
-        begin
-          rows = @client.database_query(database_id: database.database_id,
-                                        page_size: 100, start_cursor: cursor)
-        rescue Notion::Api::Errors::ObjectNotFound
-          database.destroy
-          return 
-        end
+    private
 
-        unless rows.has_more
-          return rows.results
-        end
-
-        return rows.results.concat(retrieve_rows(database, cursor: rows.next_cursor))
+    def retrieve_rows(database, cursor: nil)
+      begin
+        rows = @client.database_query(database_id: database.database_id,
+                                      page_size: 100)
+      rescue Notion::Api::Errors::ObjectNotFound
+        database.destroy
+        return 
       end
 
-      def retrieve_deleted_rows(rows)
-        account_rows = NotionRow.where(notion_database: databases).pluck(:row_id)
-        notion_rows = rows.map {|r| r.id }
-        deleted_rows = account_rows - notion_rows
-        NotionRow.where(row_id: deleted_rows)
+      unless rows.has_more
+        return rows.results
       end
 
-      def compute_row_hash(row)
-        Digest::SHA256.hexdigest(row.to_h.to_json)
+      return rows.results.concat(retrieve_rows(database, cursor: rows.next_cursor))
+    end
+
+    def retrieve_deleted_rows(database, rows)
+      account_rows = NotionRow.where(notion_database: database).pluck(:row_id)
+      notion_rows = rows.map {|r| r.id }
+      deleted_rows = account_rows - notion_rows
+      NotionRow.where(row_id: deleted_rows)
+    end
+
+    def compute_row_hash(row)
+      Digest::SHA256.hexdigest(row.to_h.to_json)
+    end
+
+    def send_row_created_event(database, row, hash)
+      ActiveRecord::Base.transaction do 
+        NotionRow.create!(notion_database: database, 
+                          row_id: row.id, object_hash: hash)
+        Webhook.create!(account: @account, 
+                        event_type: "database.row.created", payload: row.to_h)
       end
+    end
 
-      def send_row_created_event(database, row, hash)
-        ActiveRecord::Base.transaction do 
-          NotionRow.create!(notion_database: database, 
-                            row_id: row.id, object_hash: hash)
-          Webhook.create!(account: @account, 
-                          event_type: "database.row.created", payload: row.to_h)
-        end
+    def send_row_updated_event(db_row, row, hash)
+      ActiveRecord::Base.transaction do 
+        update = db_row.update!(object_hash: hash)
+        Webhook.create!(account: @account, event_type: "database.row.updated",
+                        payload: row.to_h)
       end
+    end
 
-      def send_row_updated_event(db_row, row, hash)
-        ActiveRecord::Base.transaction do 
-          update = db_row.update!(object_hash: hash)
-          Webhook.create!(account: @account, event_type: "database.row.updated",
-                          payload: row.to_h
-        end
-      end
+    def send_row_deleted_event(db_row)
+      payload = {
+        id: db_row.row_id
+      }
 
-      def send_row_deleted_event(db_row)
-        payload = {
-          id: db_row.row_id
-        }
-
-        ActiveRecord::Base.transaction do 
-          Webhook.create!(account: @account, event_type: "database.row.deleted", 
-                          payload: payload)
-          db_row.destroy!
-        end
+      ActiveRecord::Base.transaction do 
+        Webhook.create!(account: @account, event_type: "database.row.deleted", 
+                        payload: payload)
+        db_row.destroy!
       end
     end
   end
--- app/jobs/poll_notion_resources_job.rb
+++ app/jobs/poll_notion_resources_job.rb
@@ -1,6 +1,6 @@
 class PollNotionResourcesJob < ApplicationJob
   queue_as :notion_poller
-  discard_on StandardError
+  #discard_on StandardError
 
   def perform(account_id)
     account = Account.find(account_id)
